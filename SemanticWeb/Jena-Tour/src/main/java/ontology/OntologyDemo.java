16520c2f67754ab10148cdc06202af90e51ef2dc
Hikmat <me.hemant.available@gmail.com>
2016-01-13 19:08:30 +0545

    adding fuseki demo

--- SemanticWeb/Jena-Tour/src/main/java/ontology/OntologyDemo.java
+++ SemanticWeb/Jena-Tour/src/main/java/ontology/OntologyDemo.java
@@ -10,15 +10,22 @@ import org.apache.jena.rdf.model.Resource;
 import java.util.Iterator;
 
 /**
- * Created by hdhamee on 12/29/15.
+ * By default, the file path_name is used for prefix name space for
+ * all the relative URIS in rdf document. If there is a xmlns:base
+ * namespace is defined then it will be used for relative URIs.
+ *
+ * xml:base="http://www.eswc2006.org/technologies/ontology
+ *
  */
 public class OntologyDemo {
     public static void main(String[] args) {
+
         // create the base model
         String SOURCE = "data/ontology.rdf";
-        String NS = SOURCE + "#";
+        String basens = "http://www.eswc2006.org/technologies/ontology";
+        String NS = basens + "#";
         OntModel base = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM );
-        base.read( SOURCE, "RDF/XML" );
+        base.read(SOURCE, "RDF/XML" );
 
         // create the reasoning model using the base
         OntModel inf = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_MICRO_RULE_INF, base);
@@ -28,14 +35,17 @@ public class OntologyDemo {
         Individual p1 = inf.createIndividual( NS + "paper1", paper );
 
         // list the asserted types
-        for (Iterator<Resource> i = p1.listRDFTypes(false); i.hasNext(); ) {
+        Iterator<Resource> i = p1.listRDFTypes(false);
+        while (i.hasNext()){
             System.out.println( p1.getURI() + " is asserted in class " + i.next() );
         }
 
         // list the inferred types
-        /*p1 = inf.getIndividual( NS + "paper1" );
-        for (Iterator<Resource> i = p1.listRDFTypes(false); i.hasNext(); ) {
-            System.out.println( p1.getURI() + " is inferred to be in class " + i.next() );
-        }*/
+        p1 = inf.getIndividual( NS + "paper1" );
+        Iterator<Resource> ii = p1.listRDFTypes(false);
+        System.out.println("\n\n");
+        while(ii.hasNext()) {
+            System.out.println( p1.getURI() + " is inferred to be in class " + ii.next() );
+        }
     }
 }
